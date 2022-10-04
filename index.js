ba346f6664ed39e19843cf8edb92452258dcbf78
Peanut1945 <Peanutinmyass@proton.me>
2022-10-04 12:49:46 +0100

    made the comparison operator

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let compnum = 0
 let compvalue = ""
+let winner = ""
+let condition = ""
 function computerchoice(){
     compnum = Math.random()
     if(compnum <= 0.3333){
@@ -12,8 +14,48 @@ function computerchoice(){
 
 }
 function comparison(playeroption,computerchoice){
+    if (playeroption == "ROCK"){
+        if (computerchoice == "SCISSORS"){
+            winner = "player wins!"
+            condition = "rock beats scissors"
+        }else if (computerchoice == "PAPER"){
+            winner = "computer wins! "
+            condition = "paper beats rock"
+        }
+        else{
+            winner = "its a tie: "
+            condition = "paper ties with paper"
+        }
+    }else if (playeroption == "SCISSORS"){
+        if (computerchoice == "PAPER"){
+            winner = "player wins! "
+            condition = "scissors beats paper"
+        }else if (computerchoice == "ROCK"){
+            winner = "computer wins! "
+            condition = "rock beats scissors"
+        }else{
+            winner = "its a tie: "
+            condition = "scissors tie with scissors"
+        }
+    }else if (playeroption == "PAPER"){
+        if (computerchoice == "ROCK"){
+            winner = "player wins! "
+            condition = "paper beats rock"
+        }else if (computerchoice == "SCISSORS"){
+            winner = "computer wins! "
+            condition = "scissors beats paper"
+        }else{
+            winner = "its a tie: "
+            condition = "rock ties with rock"
+        }
+    }
 
 }
+let play = prompt("enter rock paper or scissors")
+play = play.toUpperCase()
 computerchoice()
 console.log(compnum)
 console.log(compvalue)
+compvalue = compvalue.toUpperCase()
+comparison(play,compvalue)
+console.log(winner + condition)
